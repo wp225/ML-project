d9d45f6a11670bf26cfcc5023816b993bd409a1a
jeorgejoshi <85611495+wp225@users.noreply.github.com>
2024-05-08 14:25:57 +0545

    Deployment Configuration

---
+++ application.py
@@ -0,0 +1,44 @@
+import pickle
+from flask import Flask,request
+from flask import render_template
+import numpy as np
+import pandas as pd
+from src.pipeline.infer_pipeline import CustomData,PredictPipeline
+from sklearn.preprocessing import StandardScaler
+
+application = Flask(__name__)
+
+app=application
+
+#Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods= ['GET','POST'])
+def predict_data():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=request.form.get('reading_score'),
+            writing_score=request.form.get('writing_score'),
+        )
+        predict_df = data.get_data_as_df()
+        print(predict_df)
+
+        predict_pipeline = PredictPipeline()
+        prediction  = predict_pipeline.predict(predict_df)
+
+        return render_template('home.html',results = prediction[0])
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
+
